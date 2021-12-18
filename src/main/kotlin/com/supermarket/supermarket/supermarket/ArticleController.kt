84511de731487da3fd383e1c9908bd1d1d8c57bc
96Octavian <gogol.logog@hotmail.it>
2021-12-18 16:31:38 +0100

    Add logging to ArticleController.kt

--- src/main/kotlin/com/supermarket/supermarket/supermarket/ArticleController.kt
+++ src/main/kotlin/com/supermarket/supermarket/supermarket/ArticleController.kt
@@ -1,20 +1,25 @@
 package com.supermarket.supermarket.supermarket
 
+import org.hibernate.annotations.common.util.impl.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
+import org.jboss.logging.Logger
 
 @RestController
 @RequestMapping(value = ["api/v1/supermarket"])
 class ArticleController @Autowired constructor(private val articleService: ArticleService) {
 
+    private val logger : Logger = LoggerFactory.logger(this::class.java)
     @GetMapping
     fun listArticles(): List<Article> {
+        logger.info("Executing listArticles()")
         return articleService.listArticles()
     }
 
     @PostMapping
     fun newArticle(@RequestBody newArticle : Article) {
-        articleService.addNewArticle(newArticle)
+        logger.info("Executing newArticle()")
+        return articleService.addNewArticle(newArticle)
     }
 
 }
